dbb676ba316ed49e76e3d9b1a85fc10e7b36fab2
Brad Williams <brad.williams198@gmail.com>
2022-01-24 14:33:02 -0500

    README.md

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Home msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: white;
+  /* margin-top: 60px; */
+}
+body{
+  background: #080808;
+}
+</style>
